4bb3a94fa511915ef60e2f21a1ac38f2bd6c49b5
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-10 16:19:12 -0400

    morestuff

---
+++ portfolio4/DataContext/ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using portfolio4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace portfolio4.DataContext
+{
+    public class ProjectContext :DbContext
+    {
+        public ProjectContext():base()
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ portfolio4/Migrations/201705101928286_firstie.cs
@@ -0,0 +1,28 @@
+namespace portfolio4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstie : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        gitUrl = c.String(),
+                        liveUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Projects");
+        }
+    }
+}
