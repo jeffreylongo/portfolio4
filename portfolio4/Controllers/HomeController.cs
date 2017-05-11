4783367b943fbff58511ffcfd9e2ef83d6e32024
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-11 13:48:30 -0400

    set up dbcontext and controller for initial get...not sure if works.

--- portfolio4/Controllers/HomeController.cs
+++ portfolio4/Controllers/HomeController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using portfolio4.Models;
+using portfolio4.DataContext;
 
 namespace portfolio4.Controllers
 {
     public class HomeController : Controller
     {
+        private ProjectContext db = new ProjectContext();
 
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            Project project = db.Projects.Find(id);
+            return View(project);
         }
 
         public ActionResult About()
