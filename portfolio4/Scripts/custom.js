34a7d60cfbc5521d2f9c256604c4cc4a8a0905c2
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-11 20:39:59 -0400

    all snippets in db and showing

---
+++ portfolio4/Scripts/custom.js
@@ -0,0 +1,4 @@
+﻿$("#pop").on("click", function () {
+    $('#imagepreview').attr('src', $('#imageresource').attr('@project.projectImage')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
